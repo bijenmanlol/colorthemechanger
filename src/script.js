9af55f08806ce673e178cbb5ef08381969d4e452
drooler <85669046+bijenmanlol@users.noreply.github.com>
2024-03-07 14:43:28 +0100

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,35 @@
+// elements var
+root = document.getElementsByTagName("html")[0];
+head = document.getElementsByTagName("head")[0];
+
+// adding stylesheet and button
+head += '<link rel="stylesheet" href="https://bijenmanlol.github.com/colorthemechanger/src/style.css" type="text/css">';
+root = '<button id="color-scheme-selector">LIGHT MODE</button>' + root;
+
+// element var
+root = document.getElementsByTagName("html")[0];
+
+// saving color scheme in local storage
+if(localStorage.getItem("current-color-scheme") == "light") {
+    root.setAttribute("current-color-scheme", "light");
+    button.innerHTML = "DARK MODE";
+    localStorage.setItem("current-color-scheme", "light");
+}else {
+    root.setAttribute("current-color-scheme", "dark");
+    button.innerHTML = "LIGHT MODE";
+    localStorage.setItem("current-color-scheme", "dark");
+}
+
+// changing color theme
+button.addEventListener("click", function() {
+    if(root.getAttribute("current-color-scheme") == "dark") {
+        root.setAttribute("current-color-scheme", "light");
+        button.innerHTML = "DARK MODE";
+        localStorage.setItem("current-color-scheme", "light");
+    }else {
+        root.setAttribute("current-color-scheme", "dark");
+        button.innerHTML = "LIGHT MODE";
+        localStorage.setItem("current-color-scheme", "dark");
+    }
+})
+
